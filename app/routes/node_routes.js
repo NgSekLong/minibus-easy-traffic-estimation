cacec5f07ec0d5187365561af62ea691fc356f32
NgSekLong <longsek@gmail.com>
2019-04-05 17:41:24 +0800

    refractor

--- app/routes/node_routes.js
+++ app/routes/node_routes.js
@@ -12,34 +12,9 @@ const googleMapsClient = require('@google/maps').createClient({
 // routes/note_routes.js
 module.exports = function(app, db) {
   app.post('/passenger_request_arrival_time', async(req, res) => {
-    // You'll create your note here.
+    var param = req.body;
     var arrivalTimeInfo = await google_map_helper.request_arrival_time();
-     //res.body('yup');
      res.send(arrivalTimeInfo);
 
-     //var return_data = await google_map_helper.request_arrival_time();
-    // console.log(return_data);
-    // console.log(return_data);
-    // res.send(JSON.stringify(return_data));
   });
 };
-
-function callGoogleAPI() {
-  return googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'})
-    .asPromise()
-    .then((response) => {
-      return response.json.results;
-      //console.log(response.json.results);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-async function sleep(fn, ...args) {
-    await timeout(3000);
-    return fn(...args);
-}
