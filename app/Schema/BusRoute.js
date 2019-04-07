8fe0e76ac8d00135bd4aae1fb6ac6e86039a1781
NgSekLong <longsek@gmail.com>
2019-04-07 18:21:26 +0800

    Bus route allow list buese

--- app/Schema/BusRoute.js
+++ app/Schema/BusRoute.js
@@ -7,9 +7,14 @@ const BusRoute = new Schema({
   id: ObjectId,
   route_id: String,
   route_number: String,
-  bus_routes : {
-    bus_stops: Object
-  },
+  region: String,
+  bus_routes : [{
+    bus_stops: Object,
+    route_start_at_en: String,
+    route_start_at_tc: String,
+    route_end_at_en: String,
+    route_end_at_tc: String,
+  }],
 });
 
 
--- app/routes/node_routes.js
+++ app/routes/node_routes.js
@@ -21,6 +21,7 @@ module.exports = function(app, db) {
     var query = req.query; // $_GET["id"]
 
 		if(!query.direction){
+
 			res.send("error");
 			return;
 		}
@@ -34,8 +35,12 @@ module.exports = function(app, db) {
 
   app.post('/passenger_request_arrival_real_time', async(req, res) => {
 
+    // res.setHeader('Content-Type', 'application/json');
+    // res.send('[{"title":"123"}]');
+    // return;
     var param = req.body;
 		if(!param.route_id){
+      res.sendStatus(404)
 			res.send("error");
 			return;
 		}
@@ -54,6 +59,103 @@ module.exports = function(app, db) {
 	    var arrivalTimeInfo = await google_map_helper.request_arrival_time(bus_stops);
 			console.log(arrivalTimeInfo);
 			res.send(arrivalTimeInfo);
+		  // docs.forEach
+		});
+
+
+  // BusRouteModel.find({}, function(err, users) {
+	// 	console.log(users);
+  //   var userMap = {};
+	//
+  //   users.forEach(function(user) {
+  //     userMap[user._id] = user;
+  //   });
+	//
+  //   res.send(userMap);
+  // });
+
+    // var param = req.body;
+    // var arrivalTimeInfo = await google_map_helper.request_arrival_time();
+    //  res.send(arrivalTimeInfo);
+
+  });
+
+
+
+  app.post('/list_bueses', async(req, res) => {
+
+    // res.setHeader('Content-Type', 'application/json');
+    // res.send('[{"title":"123"}]');
+    // return;
+    // var param = req.body;
+		// if(!param.route_id){
+    //   res.sendStatus(404)
+		// 	res.send("error");
+		// 	return;
+		// }
+		mongoose.connect(MONGODB_CONNECTION_URI, {useNewUrlParser: true});
+
+		i = 0
+
+    let returnBuses = [];
+		const BusRouteModel = mongoose.model(MONGODB_BUS_COLLECTION, BusRoute);
+		BusRouteModel.find({}, async function (err, buses) {
+
+			if(buses.length == 0){
+				res.send('No Route');
+				return;
+			}
+
+      buses.forEach(function(bus) {
+
+        let busRoutes = bus['bus_routes'];
+        //console.log(busRoutes.count());
+        //asd
+
+        // for(let i =0; i < busRoutes.length; i ++){
+        //     bus_route = busRoutes[i];
+        //
+        //     console.log('bus_route', bus_route);
+        //     return ;
+        //
+        //     let route_id = bus.route_id;
+        //     let route_number = bus.route_number;
+        //     let region = bus.region;
+        //
+        //     let route_start_at_en = bus_route.route_start_at_en;
+        //     let route_start_at_tc = bus_route.route_start_at_tc;
+        //
+        //
+        //     let route_end_at_en = bus_route.route_end_at_en;
+        //     let route_end_at_tc = bus_route.route_end_at_tc;
+        //     returnBuses.push({route_id, route_number, region, route_start_at_en, route_start_at_tc, route_end_at_en, route_end_at_tc})
+        //
+        //
+        // }
+        // console.log('something?');
+        // console.log(returnBuses);
+        // return;
+
+        busRoutes.forEach(function(bus_route) {
+          let route_id = bus.route_id;
+          let route_number = bus.route_number;
+          let region = bus.region;
+
+          let route_start_at_en = bus_route.route_start_at_en;
+          let route_start_at_tc = bus_route.route_start_at_tc;
+          
+          let route_end_at_en = bus_route.route_end_at_en;
+          let route_end_at_tc = bus_route.route_end_at_tc;
+
+          returnBuses.push({route_id, route_number, region, route_start_at_en, route_start_at_tc, route_end_at_en, route_end_at_tc})
+        });
+      });
+
+			// var bus_stops = bus[0]['bus_routes']['bus_stops'][0]//.bus_routes[i].bus_stops;
+      //
+	    // var arrivalTimeInfo = await google_map_helper.request_arrival_time(bus_stops);
+			// console.log(arrivalTimeInfo);
+			res.send(returnBuses);
 		  // docs.forEach
 		});
 
